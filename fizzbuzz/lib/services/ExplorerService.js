81bbaeb44499395385e9eed7fbd1cf019c462244
MisaelFloresB <misaelf82@gmail.com>
2022-04-27 17:47:06 -0500

    Se agrego services y el archivo ExplorerService.js junto con sus respectivos metodos

---
+++ fizzbuzz/lib/services/ExplorerService.js
@@ -0,0 +1,25 @@
+const Reader = require('./../utils/Reader')
+
+class ExplorerService{
+    static filterByMission(explorers, mission){
+        const explorersByMision = explorers.filter((explorer) => explorer.mission === mission)
+        return explorersByMision
+    }
+
+    static getAmountOfExplorersByMission(explorers, mission){
+        const amountOfExplorersByMission = explorers.filter((explorer) => explorer.mission === mission)
+        return amountOfExplorersByMission
+
+
+    }
+
+    static getExplorersUsernamesByMission(explores, mission){
+        const explorersUsernameByMission = explores.filter((explorer) => explorer.mission === mission);
+        const explorersUsername = explorersUsernameByMission.map((explorer) => explorer.githubUsername);
+
+        return explorersUsername
+
+    }
+}
+
+module.export = ExplorerService
