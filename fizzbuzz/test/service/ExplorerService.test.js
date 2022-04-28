8af1a230e5b789ef8422416ddf5657364f891dd4
MisaelFloresB <misaelf82@gmail.com>
2022-04-27 21:18:26 -0500

    Se agrego el test para el metodo getExplorersUsernamesByMission

--- fizzbuzz/test/service/ExplorerService.test.js
+++ fizzbuzz/test/service/ExplorerService.test.js
@@ -21,6 +21,20 @@ describe("Unit test for ExplorerService, filterByMission", () =>{
         const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersInNode).toBe(1)
 
-    })
+    });
+
+    test("Unit tesr for ExplorerService, getExplorersUsernamesByMission", () =>{
+
+        const explorers = [{
+            name: "Woopa15",
+            githubUsername: "ajolonauta15",
+            score: 15,
+            mission: "node",
+        }];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode).toBe("ajolonauta15");
+
+    });
+    
 
 });
