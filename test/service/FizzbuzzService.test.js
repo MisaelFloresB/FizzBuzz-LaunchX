970db3f382ab850f5b4cb48fe274c5c28e6f0bc2
MisaelFloresB <misaelf82@gmail.com>
2022-04-28 13:27:03 -0500

    Se reestructuro la ruta de los archivos

---
+++ test/service/FizzbuzzService.test.js
@@ -0,0 +1,28 @@
+const FizzBuzzService = require('./../../lib/services/FizzbuzzService')
+const Reader = require('./../../lib/utils/Reader')
+
+describe("Unit test for FizzBuzzService, applyValidationInExplorer", () =>{
+    test("Test for requirement FIZZ", () =>{
+        const explorer1 = {name: "Woopa1", score: 6};
+
+        const explorersFizzbuzz = FizzBuzzService.applyValidationInExplorer(explorer1);
+
+        expect(explorersFizzbuzz.trick).toEqual("FIZZ")
+        });
+
+        test("Test for requirement BUZZ", () =>{
+            const explorer2 = {name: "Woopa1", score: 5};
+    
+            const explorersFizzbuzz = FizzBuzzService.applyValidationInExplorer(explorer2);
+    
+            expect(explorersFizzbuzz.trick).toEqual("BUZZ")
+        });
+
+        test("Test for requirement FIZZBUZZ", () =>{
+            const explorer3 = {name: "Woopa1", score: 15};
+    
+            const explorersFizzbuzz = FizzBuzzService.applyValidationInExplorer(explorer3);
+    
+            expect(explorersFizzbuzz.trick).toEqual("FIZZBUZZ")
+        });
+    });
