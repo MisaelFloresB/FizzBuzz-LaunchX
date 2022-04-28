ac2909e9b9fa31558bb9cf6e399f62d4ab70278b
MisaelFloresB <misaelf82@gmail.com>
2022-04-27 19:22:26 -0500

    Se agrego test para getAmountOfExplorersByMission

--- fizzbuzz/test/service/ExplorerService.test.js
+++ fizzbuzz/test/service/ExplorerService.test.js
@@ -1,6 +1,6 @@
 const ExplorerService = require('./../../lib/services/ExplorerService');
 
-describe("Unit test for ExplorerService", () =>{
+describe("Unit test for ExplorerService, filterByMission", () =>{
     test("Test for requirement 1", () =>{
         const explorers = [{
             name: "Woopa15",
@@ -11,7 +11,16 @@ describe("Unit test for ExplorerService", () =>{
 
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
 
-        expect(explorersInNode.length).toBe(1);
+        expect(explorersInNode).toEqual(explorers);
     });
+
+    test("Unit test for ExplorerService, getAmountOfExplorersByMission", () =>{
+
+        const explorers = [{name: "Woopa14", mission:"node"}]
+
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(explorersInNode).toBe(1)
+
+    })
 
 });
