f37a5765682137cab1551b3a05ab920ef3d1934c
MisaelFloresB <misaelf82@gmail.com>
2022-04-29 13:34:39 -0500

    Se agrega test para getExplorersUsernamesByMission y getExplorersAmonutByMission

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,12 +1,21 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
-// const Reader = require("./../../lib/utils/Reader");
 
-describe("Unit test for ExplorerController, getExplorersByMission", () =>{
-    test("Test for requirement 1", () =>{
-        // const explorers = Reader.ReadJsonFile("data/explorers.json");
-
+describe("Test for controller server", () =>{
+    test("Unit test for ExplorerController, getExplorersByMission", () =>{
         const explorersInNode = ExplorerController.getExplorersByMission("node");
 
         expect(explorersInNode.length).toEqual(10);
+    });
+
+    test("Unit test for ExplorerController, getExplorersUsernamesByMission", () =>{
+        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission("node");
+
+        expect(explorersUsernames).toEqual(expect.arrayContaining(explorersUsernames));
+    });
+
+    test("Unit test for ExplorerController, getExplorersAmonutByMission", () =>{
+        const explorersUsernames = ExplorerController.getExplorersAmonutByMission("java");
+
+        expect(explorersUsernames).toBe(5);
     });
 });
